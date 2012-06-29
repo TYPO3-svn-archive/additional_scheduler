28038fa738e877ab8e2ecb050cd73b3e4a8f42fb
ycerdan <ycerdan@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-29 18:17:08 +0000

    Update to TER version 1.0.7 Add cleart3temp task
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/additional_scheduler/trunk@64066 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "additional_scheduler".
 #
-# Auto generated 29-02-2012 22:17
+# Auto generated 29-06-2012 20:27
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.5',
+	'version' => '1.0.7',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.0.0-0.0.0',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"a4a1";s:16:"ext_autoload.php";s:4:"5d7d";s:12:"ext_icon.gif";s:4:"b5e1";s:17:"ext_localconf.php";s:4:"e57d";s:13:"locallang.xml";s:4:"8118";s:46:"classes/class.tx_additionalscheduler_utils.php";s:4:"e20f";s:14:"doc/manual.sxw";s:4:"fdf3";s:24:"sh/save_typo3_website.sh";s:4:"eca6";s:49:"tasks/class.tx_additionalscheduler_clearcache.php";s:4:"3ed5";s:56:"tasks/class.tx_additionalscheduler_clearcache_fields.php";s:4:"55c5";s:43:"tasks/class.tx_additionalscheduler_exec.php";s:4:"8185";s:50:"tasks/class.tx_additionalscheduler_exec_fields.php";s:4:"5b3d";s:50:"tasks/class.tx_additionalscheduler_savewebsite.php";s:4:"f2d1";s:57:"tasks/class.tx_additionalscheduler_savewebsite_fields.php";s:4:"5256";s:56:"tasks/class.tx_additionalscheduler_translationupdate.php";s:4:"16ec";s:63:"tasks/class.tx_additionalscheduler_translationupdate_fields.php";s:4:"7003";}',
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"a5d8";s:16:"ext_autoload.php";s:4:"5d7d";s:12:"ext_icon.gif";s:4:"b5e1";s:17:"ext_localconf.php";s:4:"e57d";s:13:"locallang.xml";s:4:"5f25";s:46:"classes/class.tx_additionalscheduler_utils.php";s:4:"fdfd";s:14:"doc/manual.sxw";s:4:"fdf3";s:24:"sh/save_typo3_website.sh";s:4:"eca6";s:49:"tasks/class.tx_additionalscheduler_clearcache.php";s:4:"3ed5";s:56:"tasks/class.tx_additionalscheduler_clearcache_fields.php";s:4:"55c5";s:50:"tasks/class.tx_additionalscheduler_cleart3temp.php";s:4:"8559";s:57:"tasks/class.tx_additionalscheduler_cleart3temp_fields.php";s:4:"c4f4";s:43:"tasks/class.tx_additionalscheduler_exec.php";s:4:"8185";s:50:"tasks/class.tx_additionalscheduler_exec_fields.php";s:4:"5b3d";s:50:"tasks/class.tx_additionalscheduler_savewebsite.php";s:4:"f2d1";s:57:"tasks/class.tx_additionalscheduler_savewebsite_fields.php";s:4:"5256";s:56:"tasks/class.tx_additionalscheduler_translationupdate.php";s:4:"a0dc";s:63:"tasks/class.tx_additionalscheduler_translationupdate_fields.php";s:4:"7003";}',
 	'suggests' => array(
 	),
 );
